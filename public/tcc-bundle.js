b86e5edcd41d055bddc63efc8621e0672b9b5d9c
ARULDASS R <aruldassad1@gmail.com>
2025-08-17 07:54:46 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- public/tcc-bundle.js
+++ public/tcc-bundle.js
@@ -1,201 +1,80 @@
 
-window.TCC = (() => {
-    var Module = {};
-    function b64ToByteArray(base64) {
-        var binary_string = window.atob(base64);
-        var len = binary_string.length;
+window.TCC = (function() {
+    "use strict";
+
+    function b64_to_uint8(b64) {
+        var str = atob(b64.trim()); // Trim whitespace
+        var len = str.length;
         var bytes = new Uint8Array(len);
         for (var i = 0; i < len; i++) {
-            bytes[i] = binary_string.charCodeAt(i);
+            bytes[i] = str.charCodeAt(i);
         }
         return bytes;
     }
-    Module.init = (wasm_b64_string) => {
-        return new Promise(resolve => {
-            const wasm_binary = b64ToByteArray(wasm_b64_string);
-            WebAssembly.compile(wasm_binary).then(WasmModule => {
-                var TCC = {
-                    module: WasmModule,
-                    stdout_lines: [],
-                    stdin_queue: [],
-                    on_stdout: function (c) {
-                        if (c === 10) {
-                            this.stdout_lines.push('');
-                        } else {
-                            if (this.stdout_lines.length == 0) this.stdout_lines.push('');
-                            this.stdout_lines[this.stdout_lines.length-1] += String.fromCharCode(c);
-                        }
-                    },
-                    set_stdout: function (callback) {
-                        this.on_stdout = callback;
-                    },
-                    on_stderr: function (c) { this.on_stdout(c) },
-                    set_stderr: function (callback) {
-                        this.on_stderr = callback;
-                    },
-                    on_stdin: function () {
-                        if(this.stdin_queue.length > 0)
-                            return this.stdin_queue.shift();
-                        return null;
-                    },
-                    set_stdin: function (callback) {
-                        this.on_stdin = callback;
-                    },
-                    compile: function (source) {
-                        this.stdout_lines = [];
-                        var memory = new WebAssembly.Memory({ initial: 256 });
-                        var HEAP8 = new Int8Array(memory.buffer);
-                        var HEAPU8 = new Uint8Array(memory.buffer);
-                        var HEAP16 = new Int16Array(memory.buffer);
-                        var HEAPU16 = new Uint16Array(memory.buffer);
-                        var HEAP32 = new Int32Array(memory.buffer);
-                        var HEAPU32 = new Uint32Array(memory.buffer);
-                        var HEAPF32 = new Float32Array(memory.buffer);
-                        var HEAPF64 = new Float64Array(memory.buffer);
-                        var STACKTOP = 0;
-                        var STACK_MAX = memory.buffer.byteLength;
-                        var instance = new WebAssembly.Instance(this.module, {
-                            env: {
-                                memory: memory,
-                                __memory_base: 0,
-                                __table_base: 0,
-                                stackSave: (label) => STACKTOP,
-                                stackRestore: (label) => { },
-                                _sbrk: (len) => {
-                                    var old_top = STACKTOP;
-                                    STACKTOP += len;
-                                    return old_top;
-                                },
-                                ___seterrno: (a) => { },
-                                _write: (fd, buf, len) => {
-                                    for (var i = 0; i < len; ++i) {
-                                        this.on_stdout(HEAPU8[buf + i]);
-                                    }
-                                    return len;
-                                },
-                                _read: (fd, buf, len) => {
-                                    var ret = 0;
-                                    for (var i = 0; i < len; ++i) {
-                                        var c = this.on_stdin();
-                                        if (c === null) break;
-                                        HEAPU8[buf + i] = c;
-                                        ret = i;
-                                    }
-                                    return ret;
-                                },
-                                _close: (fd) => 0,
-                                _lseek: (fd, a, b) => 0,
-                                _fstat: (fd, a) => 0,
-                            }
-                        });
-                        var source_ptr = instance.exports.malloc(source.length + 1);
-                        for (var i = 0; i < source.length; i++) {
-                            HEAPU8[source_ptr + i] = source.charCodeAt(i);
-                        }
-                        HEAPU8[source_ptr + source.length] = 0;
-                        var argv_ptr = instance.exports.malloc(4 * 4);
-                        var argv0_ptr = instance.exports.malloc(4);
-                        HEAPU8[argv0_ptr] = 0;
-                        var argv1_ptr = instance.exports.malloc(3);
-                        HEAPU8[argv1_ptr] = 45;
-                        HEAPU8[argv1_ptr + 1] = 114;
-                        HEAPU8[argv1_ptr + 2] = 0;
-                        var argv2_ptr = instance.exports.malloc(3);
-                        HEAPU8[argv2_ptr] = 45;
-                        HEAPU8[argv2_ptr + 1] = 111;
-                        HEAPU8[argv2_ptr + 2] = 0;
-                        HEAP32[(argv_ptr >> 2)] = argv0_ptr;
-                        HEAP32[(argv_ptr >> 2) + 1] = argv1_ptr;
-                        HEAP32[(argv_ptr >> 2) + 2] = argv2_ptr;
-                        HEAP32[(argv_ptr >> 2) + 3] = source_ptr;
-                        instance.exports.tcc_main(4, argv_ptr);
-                        var mem_ptr = HEAPU32[2];
-                        var mem_len = HEAPU32[3];
-                        var reloc_ptr = HEAPU32[4];
-                        var reloc_len = HEAPU32[5];
-                        var new_mem = new Uint8Array(mem_len);
-                        for(var i=0; i<mem_len; ++i) {
-                            new_mem[i] = HEAPU8[mem_ptr + i];
-                        }
-                        var relocs = [];
-                        for(var i=0; i<reloc_len/4; ++i) {
-                            relocs.push(HEAPU32[reloc_ptr/4 + i]);
-                        }
-                        this.reloc_ptr = reloc_ptr;
-                        this.relocs = relocs;
-                        this.mem = new_mem;
-                        this.mem_len = mem_len;
-                    },
-                    run: function(args) {
-                        this.stdout_lines = [];
-                        this.stdin_queue = [];
-                        var memory = new WebAssembly.Memory({ initial: 256 });
-                        var HEAP8 = new Int8Array(memory.buffer);
-                        var HEAPU8 = new Uint8Array(memory.buffer);
-                        var HEAP16 = new Int16Array(memory.buffer);
-                        var HEAPU16 = new Uint16Array(memory.buffer);
-                        var HEAP32 = new Int32Array(memory.buffer);
-                        var HEAPU32 = new Uint32Array(memory.buffer);
-                        var HEAPF32 = new Float32Array(memory.buffer);
-                        var HEAPF64 = new Float64Array(memory.buffer);
-                        var DYNAMICTOP = 0;
-                        for(var i=0; i<this.mem_len; ++i) {
-                            HEAPU8[i] = this.mem[i];
-                        }
-                        for(var i=0; i<this.relocs.length; ++i) {
-                            var reloc = this.relocs[i];
-                            var type = reloc & 0xff;
-                            var offset = reloc >> 8;
-                            var sym_index = HEAPU32[offset/4];
-                            if(sym_index == 1) { // __global_base
-                                HEAPU32[offset/4] = 0;
-                            } else if (sym_index == 2) { // HEAP_BASE
-                                HEAPU32[offset/4] = 0;
-                            }
-                        }
-                        var instance = new WebAssembly.Instance(this.module, {
-                            env: {
-                                memory: memory,
-                                __memory_base: 0,
-                                __table_base: 0,
-                                _exit: (code) => { throw 'exit'; },
-                                ___seterrno: (a) => { },
-                                _sbrk: (len) => {
-                                    var old_top = DYNAMICTOP;
-                                    DYNAMICTOP += len;
-                                    return old_top;
-                                },
-                                _write: (fd, buf, len) => {
-                                    for (var i = 0; i < len; ++i) {
-                                        this.on_stdout(HEAPU8[buf + i]);
-                                    }
-                                    return len;
-                                },
-                                _read: (fd, buf, len) => {
-                                    var ret = 0;
-                                    for (var i = 0; i < len; ++i) {
-                                        var c = this.on_stdin();
-                                        if (c === null) break;
-                                        HEAPU8[buf + i] = c;
-                                        ret = i;
-                                    }
-                                    return ret;
-                                },
-                                _close: (fd) => 0,
-                                _lseek: (fd, a, b) => 0,
-                                _fstat: (fd, a) => 0,
-                            }
-                        });
-                        try {
-                            instance.exports.main(0, 0);
-                        } catch(e) {
-                            if(e !== 'exit') throw e;
-                        }
-                    }
-                };
-                resolve(TCC);
-            });
+
+    var TCC = function() {
+        this.exports = {};
+    };
+
+    TCC.prototype.init = function(b64_string) {
+        var self = this;
+        var bytes = b64_to_uint8(b64_string);
+        return WebAssembly.instantiate(bytes, {
+            env: {
+                __eqsf2: function() {},
+                __eqdf2: function() {},
+                __addsf3: function() {},
+                __adddf3: "adddf3",
+                __subsf3: function() {},
+                __subdf3: function() {},
+                __extendsfdf2: function() {},
+                __truncdfsf2: function() {},
+                __fixsfsi: function() {},
+                __fixdfsi: function() {},
+                __fixunssfsi: function() {},
+                __fixunsdfsi: function() {},
+                __floatsisf: function() {},
+                __floatsidf: function() {},
+                __floatunsisf: function() {},
+                __floatunsidf: function() {},
+                __mulsf3: function() {},
+                __muldf3: function() {},
+                __divsf3: function() {},
+                __divdf3: function() {},
+                __gesf2: function() {},
+                __gedf2: function() {},
+                __ltsf2: function() {},
+                __ltdf2: function() {},
+                emscripten_asm_const_int: function() {},
+                emscripten_asm_const_double: function() {},
+            },
+        }).then(function(result) {
+            self.exports = result.instance.exports;
+            self.memory = self.exports.memory;
+            return self;
         });
     }
-    return Module;
+
+    TCC.prototype.compile = function(prog) {
+        var exit_code;
+        this.exports.main();
+        exit_code = this.exports.tcc_compile_string(prog);
+        return exit_code;
+    }
+
+    TCC.prototype.run = function() {
+        var exit_code;
+        this.exports.main();
+        exit_code = this.exports.tcc_run_string();
+        return exit_code;
+    }
+
+    TCC.prototype.set_stdout = function(func) {
+        this.exports.set_stdout(func);
+    }
+    TCC.prototype.set_stderr = function(func) {
+        this.exports.set_stderr(func);
+    }
+    
+    return new TCC();
 })();
